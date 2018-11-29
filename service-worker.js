bf49ee1321a6a481f2e3602cb9cfa5dd1f2443f3
Jake Lauer <jake.lauer27@gmail.com>
2018-11-28 20:16:12 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/css-animate/precache-manifest.318485b778f88375a66bcefcf7ad5b3d.js"
+  "css-animate.com/precache-manifest.cfd2b243fc75558d009f2d62b72b7c90.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/css-animate/index.html", {
+workbox.routing.registerNavigationRoute("css-animate.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
